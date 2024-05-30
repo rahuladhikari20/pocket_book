2d04d9e2b32aa2d7d59ebdcec7ab5c49e2b7b924
rahuladhikari20 <140904397+rahuladhikari20@users.noreply.github.com>
2024-05-30 06:11:00 +0530

    pass_generator.py

--- pass_generator.py
+++ pass_generator.py
@@ -34,6 +34,8 @@ def generate_password(length=16, nums=1, special_chars=1, uppercase=1, lowercase
             break
     
     return password
-if __name__=='__main__':    
+def main():    
     new_password = generate_password()
     print('Generated password:', new_password)
+if __name__=='__main__':
+    main()
